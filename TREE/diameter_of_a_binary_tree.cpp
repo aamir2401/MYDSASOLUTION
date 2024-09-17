30fe2e7b7d08bcd519fcfd549ece56c1f6a5e529
aamir2401 <aamir.anwar2401@gmail.com>
2024-09-17 23:43:48 +0530

    Diameter of a Binary Tree

---
+++ TREE/diameter_of_a_binary_tree.cpp
@@ -0,0 +1,22 @@
+ int height (Node* root)
+    {
+        if(root == NULL)
+        return 0;
+        int left = height(root -> left);
+        int right = height(root -> right);
+        int ans = max(left,right)+1;
+        return ans;
+    }
+    
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+      if(root == NULL)
+      return 0;
+      int dOfleftsub = diameter(root -> left);
+      int dOfrightsub = diameter(root -> right);
+      int mixDofTree = height(root -> left) + height (root -> right) + 1;
+      int finalAns = max(mixDofTree,max(dOfleftsub,dOfrightsub));
+      return finalAns;
+    }
+};
