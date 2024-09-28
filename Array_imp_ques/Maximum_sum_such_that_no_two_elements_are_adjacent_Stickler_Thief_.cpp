e877b7d8e46e821afa3b501aaadb65944bb9f709
aamir2401 <aamir.anwar2401@gmail.com>
2024-09-28 14:45:57 +0530

    Maximum sum such that no two elements are adjacent (Stickler Thief)

---
+++ Array_imp_ques/Maximum_sum_such_that_no_two_elements_are_adjacent_Stickler_Thief_.cpp
@@ -0,0 +1,34 @@
+//Maximum sum such that no two elements are adjacent (Stickler Thief)
+
+#include <vector>
+#include <iostream>
+#include<limits.h>
+using namespace std;
+void get_solution(vector<int> &arr, int i, int n, int sum, int &maxi)
+{
+    if(i >= n)
+    {
+        maxi = max(sum, maxi);
+        return;
+    }
+    //inclusion recursive call;
+    get_solution(arr, i + 2, n, sum + arr[i], maxi);
+
+
+    //exclusion recursive call;
+    get_solution(arr, i + 1, n, sum, maxi);
+}
+int main()
+{
+   vector<int> arr{5, 5, 10, 100, 10, 5};
+   int i = 0;
+   int n = arr.size();
+   int sum = 0;
+   int maxi = INT_MIN;
+   get_solution(arr, i, n, sum, maxi);
+
+   cout<<"We have got "<<maxi<<" as maximum non-adjacent sum"<<endl;
+
+
+    return 0;
+}
