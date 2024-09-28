dd241afdaf5c8fc2da38d4911080de49815abc35
aamir2401 <aamir.anwar2401@gmail.com>
2024-09-28 11:01:58 +0530

    printing maximum in k sized sub-array

---
+++ Array_imp_ques/max_in_K_sized_subarray.cpp
@@ -0,0 +1,27 @@
+//printing maximum in k sized sub-array
+#include<iostream>
+#include<vector>
+#define INT_MIN
+using namespace std;
+int main()
+{
+    vector<int> arr{1, 2, 3, 1, 4, 5, 2, 3, 6};
+    int k;
+    cout<<"Give the value of K to get maximum in K size of subarray:"<<endl;
+    cin>>k;
+    cout<<"You have given value of K as: "<<k<<endl;
+    int n = arr.size();
+     for(int i=0; i<=n-k; i++)
+     {
+        int maxi = 0;
+        for(int j=i; j<k+i; j++)
+        {
+            maxi = max(arr[j], maxi);
+        }
+        cout<<"maximin in given k as: "<<k<<" is "<<maxi<<endl;
+     }
+
+
+
+    return 0;
+}
